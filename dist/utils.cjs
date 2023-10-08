9ddbe294ce0aa7e288a3750e0202a9614d4784de
abirhussain <abir.khan.rcc@gmail.com>
2023-10-08 14:55:37 +0600

    add CheckoutUrl for commonjs

---
+++ dist/utils.cjs
@@ -0,0 +1,93 @@
+const axios = require("axios");
+
+// get request
+async function get(url, additionalHeaders) {
+	const controller = new AbortController();
+	const timeout = setTimeout(() => {
+		controller.abort();
+	}, 30 * 1000);
+	const { data } = await axios({
+		method: "get",
+		url: url,
+		headers: {
+			"content-type": "application/json",
+			Accept: "application/json",
+			...additionalHeaders,
+		},
+		signal: controller.signal,
+	});
+
+	clearTimeout(timeout);
+	if (data.errorMessage) throw new Error(data.errorMessage);
+	return data;
+}
+
+// post request
+async function post(url, payload, additionalHeaders) {
+	const controller = new AbortController();
+	const timeout = setTimeout(() => {
+		controller.abort();
+	}, 30 * 1000);
+	const { data } = await axios({
+		method: "post",
+		url: url,
+		headers: {
+			"content-type": "application/json",
+			Accept: "application/json",
+			...additionalHeaders,
+		},
+		data: {
+			...payload,
+		},
+		signal: controller.signal,
+	});
+	clearTimeout(timeout);
+	if (data.errorMessage) throw new Error(data.errorMessage);
+	return data;
+}
+
+// generate grant token
+async function generateGrantToken(
+	app_key,
+	app_secret,
+	username,
+	password,
+	baseURL
+) {
+	const payload = {
+		app_key,
+		app_secret,
+	};
+	const headers = {
+		username,
+		password,
+	};
+
+	const response = await post(`${baseURL}/token/grant`, payload, headers);
+	return response;
+}
+
+// refresh token
+async function renewToken(
+	app_key,
+	app_secret,
+	refresh_token,
+	username,
+	password,
+	baseURL
+) {
+	return post(
+		`${baseURL}/token/refresh`,
+		{
+			app_key,
+			app_secret,
+			refresh_token,
+		},
+		{
+			username,
+			password,
+		}
+	);
+}
+
+module.exports = { get, post, generateGrantToken, renewToken };
